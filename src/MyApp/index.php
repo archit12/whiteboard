988542a42dae2a1aacb96a580feb32d9ad1faeae
matahpushpam <matahpushpam@gmail.com>
2015-02-01 12:08:53 +0530

    Adds messaging ability gui

--- src/MyApp/index.php
+++ src/MyApp/index.php
@@ -5,17 +5,35 @@
 </head>
 <body>
     <div class="container" id="container">
+
     </div>
+
+    <input type="text" name="message" id="message">
+    <button onclick="chat_loader();">Submit</button>
+    <div id="on_send"></div>
+
 </body>
 <script type="text/javascript">
     var conn = new WebSocket('ws://localhost:7000');
     var container = document.getElementById("container");
+    var on_send=document.getElementById("on_send");
     conn.onopen = function(e) {
         container.innerHTML = "Connection Established";
     };
 
-    conn.onmessage = function(e) {
-        container.append(e.data);
+    // conn.onmessage = function(e) {
+
+        
+    // };
+    conn.onmessage = function (e)
+    {
+       on_send.innerHTML += e.data+ '<br>';
     };
+
+    function chat_loader()
+    {
+        var message=document.getElementById("message");
+        conn.send(message.value);
+    }
 </script>
 </html>
